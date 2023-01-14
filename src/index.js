248da3626209942c574bb5e5ed3eaab221e13e6f
V0mb4t <andrey_miller01@list.ru>
2023-01-14 12:58:04 +0100

    init: end task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+
+    if (n < 0) {
+        n = Math.abs(n)
+      }
+    
+      let str = n.toString()
+      n = Array.from(str);
+      let numReverse = n.reverse()
+    
+      return +numReverse.join('')
 }
